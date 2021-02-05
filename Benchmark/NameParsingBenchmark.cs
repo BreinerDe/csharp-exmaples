3ae881a26d2eb6c0558201509e18cb27faac2551
Dennis Breiner <dennis.breiner@trumpf.com>
2021-02-05 09:02:07 +0100

    add benchmark

---
+++ Benchmark/NameParsingBenchmark.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Benchmark
+{
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    public class NameParserBenchmarks
+    {
+        private const string FullName = "John R. R. Tolkien";
+        private static readonly NameParser Parser = new NameParser();
+
+        [Benchmark(Baseline = true)]
+        public void GetLastName()
+        {
+            Parser.GetLastName(FullName);
+        }
+
+        [Benchmark]
+        public void GetLastNameUsingSubstring()
+        {
+            Parser.GetLastNameUsingSubstring(FullName);
+        }
+
+        [Benchmark]
+        public void GetLastNameWithSpan()
+        {
+            Parser.GetLastNameWithSpan(FullName);
+        }
+    }
+    public class NameParser
+    {
+        public string GetLastName(string fullName)
+        {
+            var names = fullName.Split(" ");
+            return names.LastOrDefault() ?? string.Empty;
+        }
+
+        public string GetLastNameUsingSubstring(string fullName)
+        {
+            var lastSpaceIndex = fullName.LastIndexOf(" ", StringComparison.Ordinal);
+
+            return lastSpaceIndex == -1
+                ? string.Empty
+                : fullName.Substring(lastSpaceIndex + 1);
+        }
+
+        public ReadOnlySpan<char> GetLastNameWithSpan(ReadOnlySpan<char> fullName)
+        {
+            var lastSpaceIndex = fullName.LastIndexOf(' ');
+
+            return lastSpaceIndex == -1
+                ? ReadOnlySpan<char>.Empty
+                : fullName.Slice(lastSpaceIndex + 1);
+        }
+    }
+}
---
+++ Span-Split/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Span_Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
