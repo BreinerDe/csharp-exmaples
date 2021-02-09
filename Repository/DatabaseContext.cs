9d52fcecafb8148f387e7f82bb3d99e2c7e3a7a6
BreinerDe <46915742+BreinerDe@users.noreply.github.com>
2021-02-09 15:34:20 +0100

    Update DatabaseContext.cs

--- Repository/DatabaseContext.cs
+++ Repository/DatabaseContext.cs
@@ -6,7 +6,7 @@ namespace GenericRepository
     public class DatabaseContext : DbContext
     {
         public DbSet<Person> persons { get; set; }
-        public DbSet<Cars> books { get; set; }
+        public DbSet<Cars> cars { get; set; }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         { }
